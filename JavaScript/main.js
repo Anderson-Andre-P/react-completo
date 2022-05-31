5d18361a82b7d0554e8222842e7e12a71f351fc6
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2022-05-31 20:41:26 -0300

    commit

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,102 +1,156 @@
-// Tudo é objeto
-// Objeto literal
-const menu = {
-  selector: ".principal",
-  active() {
-    const menuElement = document.querySelector(this.selector);
-    menuElement.classList.add("active");
-  },
-};
+// // Tudo é objeto
+// // Objeto literal
+// const menu = {
+//   selector: ".principal",
+//   active() {
+//     const menuElement = document.querySelector(this.selector);
+//     menuElement.classList.add("active");
+//   },
+// };
 
-// menu.selector; //".principal";
-// menu.active(); // adicionar active ao menu
-// menu.hasOwnProperty("class"); // método herdado
+// // menu.selector; //".principal";
+// // menu.active(); // adicionar active ao menu
+// // menu.hasOwnProperty("class"); // método herdado
 
-function upperName(name) {
-  return name.toUpperCase();
-}
+// function upperName(name) {
+//   return name.toUpperCase();
+// }
 
-console.log(upperName("andre"));
+// console.log(upperName("andre"));
 
-["10", "20", "30"].map(Number); // [10, 20, 30]
+// ["10", "20", "30"].map(Number); // [10, 20, 30]
 
-const body = document.querySelector("body");
-body.classList.add("js"); // adiciona js ao body
+// const body = document.querySelector("body");
+// body.classList.add("js"); // adiciona js ao body
 
-const lowerName = function (name) {
-  return name.toLowerCase();
-};
-console.log(lowerName("ANDERSON"));
+// const lowerName = function (name) {
+//   return name.toLowerCase();
+// };
+// console.log(lowerName("ANDERSON"));
 
-const lowerCase = (name) => {
-  return name.toLowerCase();
-};
-console.log("ANDERSON");
+// const lowerCase = (name) => {
+//   return name.toLowerCase();
+// };
+// console.log("ANDERSON");
 
-const countLetters = (word) => word.length;
-console.log(countLetters("Anderson")); // 8
+// const countLetters = (word) => word.length;
+// console.log(countLetters("Anderson")); // 8
 
-// Destructuring
-function handleMouseMove(event) {
-  const clientX = event.clientX;
-  const clientY = event.clientY;
-  console.log(clientX, clientY);
-}
+// // Destructuring
+// function handleMouseMove(event) {
+//   const clientX = event.clientX;
+//   const clientY = event.clientY;
+//   console.log(clientX, clientY);
+// }
 
-// Irá definir uma constante para cada propriedade
-// dentro de event, que tiver o mesmo nome que a constante.
-function handleMouseMove(event) {
-  const { clientX, clientY } = event;
-  console.log(clientX, clientY);
-}
+// // Irá definir uma constante para cada propriedade
+// // dentro de event, que tiver o mesmo nome que a constante.
+// function handleMouseMove(event) {
+//   const { clientX, clientY } = event;
+//   console.log(clientX, clientY);
+// }
 
-// Podemos desestruturar o parâmetro
-function handleMouseMove({ clientX, clientY }) {
-  console.log(clientX, clientY);
-}
+// // Podemos desestruturar o parâmetro
+// function handleMouseMove({ clientX, clientY }) {
+//   console.log(clientX, clientY);
+// }
 
-document.documentElement.addEventListener("mousemove", handleMouseMove);
+// document.documentElement.addEventListener("mousemove", handleMouseMove);
 
-const frutas = ["Banana", "Uva"];
-const [fruta1, fruta2] = frutas;
-// fruta1 = banana
-// fruta2 = Uva
+// const frutas = ["Banana", "Uva"];
+// const [fruta1, fruta2] = frutas;
+// // fruta1 = banana
+// // fruta2 = Uva
 
-const useQuadrado = [
-  4,
-  function (lado) {
-    return 4 * lado;
-  },
-];
-const [lados, area] = useQuadrado;
+// const useQuadrado = [
+//   4,
+//   function (lado) {
+//     return 4 * lado;
+//   },
+// ];
+// const [lados, area] = useQuadrado;
 
-// Rest
-function showList(empresa, ...clients) {
-  clients.forEach((client) => {
-    console.log(client, empresa);
-  });
-}
+// // Rest
+// function showList(empresa, ...clients) {
+//   clients.forEach((client) => {
+//     console.log(client, empresa);
+//   });
+// }
 
-showList("Google", "André", "Pedro", "João");
+// showList("Google", "André", "Pedro", "João");
 
-// Spread
-// Arrays
-const numeros = [1, 4, 9, 2, 3, 6, 20];
-Math.max(...numeros); // retorna 20
+// // Spread
+// // Arrays
+// const numeros = [1, 4, 9, 2, 3, 6, 20];
+// Math.max(...numeros); // retorna 20
 
-// Transformar ArrayLike and Array
-const items = document.querySelectorAll("li");
+// // Transformar ArrayLike and Array
+// const items = document.querySelectorAll("li");
 
-// items.map() não existe, agora [...items]
-// é uma nova array, com cada elemento de items.
-[...items].map((item) => (item.innerText = "Teste"));
+// // items.map() não existe, agora [...items]
+// // é uma nova array, com cada elemento de items.
+// [...items].map((item) => (item.innerText = "Teste"));
 
-// Objetos
-const carro = {
-  cor: "azul",
-  portas: 4,
-  ano: 2020,
-};
+// // Objetos
+// const carro = {
+//   cor: "azul",
+//   portas: 4,
+//   ano: 2020,
+// };
 
-const cloneCarro = { ...carro };
-const carroEsportivo = { turbo: true, ...carro };
+// const cloneCarro = { ...carro };
+// const carroEsportivo = { turbo: true, ...carro };
+
+// module
+// import { areaQuadrado, perimetroQuadrado } from "./quadrado";
+
+// console.log(areaQuadrado(5));
+// console.log(perimetroQuadrado(5));
+
+// import numeroAleatorio from "./numeroAleatorio";
+
+// console.log(numeroAleatorio());
+
+// fetch("https://ranekapi.origamid.dev/wp-json/api/produto")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     console.log(json);
+//   });
+
+// async function fetchProdutos(url) {
+//   const response = await fetch(url);
+//   const json = await response.json();
+//   return json;
+// }
+
+// fetchProdutos("https://ranekapi.origamid.dev/wp-json/api/produto");
+
+// const precos = [
+//   "Crédito",
+//   "R$ 200",
+//   "R$ 400",
+//   "Contas Pagar",
+//   "R$ 300",
+//   "R$ 400",
+//   "Meus dados",
+// ];
+
+// // Retorna uma array nova, que contem os items em
+// // que o retorno da função for verdadeiro
+// const precosFiltro = precos.filter((preco) => preco.includes("R$"));
+
+// // Retorna uma nova array, modificada com o
+// // retorno de cada item da função
+// const precoNumeros = precosFiltro.map((preco) =>
+//   Number(preco.replace("R$ ", ""))
+// );
+
+const grupoA = 100;
+const grupoB = 300;
+const vencedor = grupoA > grupoB ? "Grupo A Venceu" : "Grupo B Venceu";
+
+const numeros = [2, 3, 4, 5, 6];
+const total = numeros.filter((numero) => numero > 4);
+
+const active = true;
+const button = active && "Botão está ativo";
